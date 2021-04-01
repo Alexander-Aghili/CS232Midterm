6ebff23f3b34eefcd02f393b4ecf38832212f0f9
Alexander Aghili <43054019+Alexander-Aghili@users.noreply.github.com>
2021-03-31 20:08:56 -0700

    Add files via upload

--- Question 6: Calendar Date/CalendarDate.java
+++ Question 6: Calendar Date/CalendarDate.java
@@ -8,16 +8,36 @@ public class CalendarDate implements Comparable<CalendarDate>{
 	public CalendarDate() {
 		this.month = 5;
 		Random rand = new Random();
-		this.day = rand.nextInt(30) + 1;
+		this.day = rand.nextInt(31) + 1;
 	}
 
+	//Made for testing, may be used if wanted. Still a random day but a set month
 	public CalendarDate(int month) {
-		if (month < 13 && month > 0)
+		if (month < 13 && month > 0) {
 			this.month = month;
-		Random rand = new Random();
-		this.day = rand.nextInt(30) + 1;
+			Random rand = new Random();
+			this.day = rand.nextInt(31) + 1;
+		}
+		else 
+			System.out.println("Bad Input. Object not created.");
 	}
 	
+	public Integer getDay() {
+		return day;
+	}
+
+	public void setDay(Integer day) {
+		this.day = day;
+	}
+
+	public Integer getMonth() {
+		return month;
+	}
+
+	public void setMonth(Integer month) {
+		this.month = month;
+	}
+
 	public boolean equals(Object obj) {
 		if (obj instanceof CalendarDate) {
 			CalendarDate otherDate = (CalendarDate) obj;
@@ -27,15 +47,17 @@ public class CalendarDate implements Comparable<CalendarDate>{
 		}
 	}
 	
+	//Returns -1 when day and month is less than this
+	//Returns 1 when day and month is greater than this
 	@Override
 	public int compareTo(CalendarDate otherDate) {
 		if(this.equals(otherDate)) {
 			return 0;
 		} else {
 			if (this.month.equals(otherDate.month)) {
-				return this.day.compareTo(otherDate.day);
+				return otherDate.day.compareTo(this.day);
 			} else {
-				return this.month.compareTo(otherDate.month);
+				return otherDate.month.compareTo(this.month);
 			}
 		}
 	}
--- Question 6: Calendar Date/CalendarDriver.java
+++ Question 6: Calendar Date/CalendarDriver.java
@@ -1,10 +1,21 @@
+import java.util.Random;
 
 public class CalendarDriver{
 
 	public static void main(String[] args) {
 		CalendarDate date = new CalendarDate();
+		System.out.println("Month: " + date.getMonth() + " Day " + date.getDay());
 		
-		CalendarDate[] dates = {new CalendarDate(3), new CalendarDate(), new CalendarDate(), new CalendarDate(6)};
+		
+		CalendarDate[] dates = new CalendarDate[100];
+		
+		for(int i = 0; i < 100; i++) {
+			Random rand = new Random();
+			int num = rand.nextInt(12) + 1;
+			CalendarDate newDate = new CalendarDate(num);
+			System.out.println("Month: " + newDate.getMonth() + " Day " + newDate.getDay());
+			dates[i] = new CalendarDate(num);
+		}
 		System.out.println(date.checkDate(dates));
 	}
 
